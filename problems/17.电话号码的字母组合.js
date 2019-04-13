397cb4b4551194de1c34d2a3c121c955547cf10b
HainSong <song_yz@buaa.edu.cn>
2019-04-13 23:27:14 +0800

    17

---
+++ problems/17.电话号码的字母组合.js
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=17 lang=javascript
+ *
+ * [17] 电话号码的字母组合
+ *
+ * https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/description/
+ *
+ * algorithms
+ * Medium (47.93%)
+ * Total Accepted:    20.8K
+ * Total Submissions: 42.9K
+ * Testcase Example:  '"23"'
+ *
+ * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+ * 
+ * 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+ * 
+ * 
+ * 
+ * 示例:
+ * 
+ * 输入："23"
+ * 输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ * 
+ * 
+ * 说明:
+ * 尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。
+ * 
+ */
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    const rels = {
+        '2': ['a', 'b', 'c'], '3': ['d', 'e', 'f'],
+        '4': ['g', 'h', 'i'], '5': ['j', 'k', 'l'],
+        '6': ['m', 'n', 'o'], '7': ['p', 'q', 'r', 's'],
+        '8': ['t', 'u', 'v'], '9': ['w', 'x', 'y', 'z']};
+    let arr = [];
+    
+    let dfs = (digits, depth, res) => {
+        if(depth == digits.length) {
+            arr.push(res);
+            return;
+        }
+        for(let i = 0; i < rels[digits[depth]].length; i++) {
+            dfs(digits, depth+1, res+rels[digits[depth]][i]);
+        }
+    };
+    
+    if(digits.length == 0)
+        return [];
+    
+    dfs(digits, 0, '');
+    return arr;
+};
+
